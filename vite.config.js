1d62e258005f4c74e6cdfdb07af9592bef80950a
RahymCharyev <r569837@gmail.com>
2023-03-16 22:35:19 +0500

    ✨feature: new commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/budget-app/", // укажите путь к вашему приложению
   build: {
-    publicDir: "docs",
+    outDir: "docs", // поменяйте publicDir на outDir
   },
 });
